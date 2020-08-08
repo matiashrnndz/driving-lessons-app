31b059600aaea9025fc4b98fb6d90e17b849a9f9
Matias Hernández <matiashrnndz@gmail.com>
2020-08-08 19:36:56 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="cache-control" content="no-cache">
+        <title>Driving Lessons</title>
+        <link rel="stylesheet" type="text/css" href="styles/body.css">
+        <link rel="stylesheet" type="text/css" href="layout/main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php
+            include('layout/header.html');
+            include('layout/sidenav.html');
+        ?>
+        <div class="main">
+            <div class="page">
+                <?php
+                    include('./blank.html');
+                ?>
+            </div>
+        </div>
+        
+    </body>
+</html>
